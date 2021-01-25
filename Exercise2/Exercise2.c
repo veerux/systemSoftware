d5b69f61c5797212bf6820637ddd183f36bc5f8c
veeru <veera.maattanen@hotmail.com>
2021-01-25 10:06:44 +0200

    Exercise 2, task 5 done

--- Exercise2/Exercise2.c
+++ Exercise2/Exercise2.c
@@ -29,7 +29,7 @@ int main()
   int sum = first + second;
   printf("The sum is %i.\n", sum);
 
-  float average = sum / 2;
+  float average = sum / (float)2;
   printf("The average is %.2f\n", average);
 
   int difference = abs(first - second);
@@ -38,7 +38,7 @@ int main()
   float division;
   if (second != 0)
   {
-    division = first / second;
+    division = first / (float)second;
     printf("The division is %.2f\n", division);
   }
 
@@ -64,8 +64,28 @@ int main()
 
   if (number % 3 == 0 && number % 8 == 0)
   {
-    printf("%i is divisible by both 3 and 8!\n", number)
+    printf("%i is divisible by both 3 and 8!\n", number);
   }
+
+
+  // TASK 5
+
+  int array[10] = {3, 14, 25, 36, 47, 58, 69, 71, 82, 93};
+
+  //printing the list
+  for(int i = 0; i < 10; i++)
+      printf("%d ", array[i]);
+  printf("\n");
+
+  //printing the list from end to beginning
+  for(int i = 9; i >= 0; i--)
+      printf("%d ", array[i]);
+  printf("\n");
+
+  //checking if the random number created earlier is in the array
+  for(int i = 0; i < 10; i++)
+    if (array[i] == number)
+      printf("The random number %i is in the array!\n", number);
 
 
 }
