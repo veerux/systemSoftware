5f5459291c98f278264e99c929e771e69697fcb7
veeru <veera.maattanen@hotmail.com>
2021-01-25 13:20:11 +0200

    Task 3b done

--- Exercise3/Exercise3.c
+++ Exercise3/Exercise3.c
@@ -10,9 +10,23 @@ int sumOfNumbers (int n)
   return sum;
 }
 
+int sumOfEven (int n)
+{
+  int sum = 0;
+  for (int i = 1; i <= n; i++)
+  {
+    if (i % 2 == 0)
+    {
+      sum = sum + i;
+    }
+  }
+  return sum;
+}
+
 
 int main()
 {
   printf("%i\n", sumOfNumbers(100));
+  printf("%i\n", sumOfEven(1000));
 
 }
